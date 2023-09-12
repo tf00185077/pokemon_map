51b8b54eafc638beceb4e59ca6261cac47592e43
unknown <tf00185077@gmail.com>
2023-09-13 00:15:34 +0800

    <First Time Build Project>

---
+++ app/poke/page.js
@@ -0,0 +1,10 @@
+import PokemMap from '../components/poke_map/poke_main_map';
+import PokeMap from '../components/poke_map/poke_main_map'
+import { Fragment } from 'react';
+
+const pokeMap = ()=>{
+    return <Fragment>
+        <PokemMap />
+    </Fragment>
+}
+export default pokeMap;
